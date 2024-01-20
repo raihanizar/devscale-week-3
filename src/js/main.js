202dcc44f08c6ed2994dc257acb500f9faaa43d4
raihannizar24 <raihannizar24@gmail.com>
2024-01-20 16:57:06 +0700

    [WIP] fungsi toggle menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+const menus = document.querySelectorAll(".menu");
+const contents = document.querySelectorAll(".content");
+menus.forEach(menu => {
+     menu.addEventListener("click", () => {
+        const selectedClass = menu.classList;
+        const category = selectedClass.item(1);
+        console.log(category);
+        // // hide semua content
+        // contents.forEach(content => content.classList.add("hidden"));
+
+        // // munculkan lagi menu yg di-klik
+        // menu.classList.remove("hiden");
+    })
+})
+
+
+
