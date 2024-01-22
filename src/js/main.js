7453d6fdd3a0214bf66c91ea972489a2d8240aa4
Raihan Nizar <raihannizar24@gmail.com>
2024-01-22 07:36:49 +0700

    feat(menu): adding responsive layout for navigating menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,49 @@
-const menus = document.querySelectorAll(".menu");
-const contents = document.querySelectorAll(".content");
-menus.forEach(menu => {
+const menuDiv = document.querySelectorAll(".menu");
+const bottomMenuDiv = document.querySelectorAll(".bottom-menu");
+const contentDiv = document.querySelectorAll(".content");
+
+menuDiv.forEach(menu => {
   menu.addEventListener("click", () => {
+    // reset scroll ke atas
+    document.documentElement.scrollTop = 0;
+
+    // get kategori menu: about / works / blog / contact
     const menuCategory = menu.classList.item(1);
+    console.log(menu.classList);
 
-    // hide semua content
-    contents.forEach(content => {
+    // hide semua content kecuali kategori yg di-klik
+    contentDiv.forEach(content => {
       let contentCategory = content.classList.item(1);
-      if (contentCategory === menuCategory) {
-        content.style.display = "block";
-      } else {
-        content.style.display = "none";
+      if (contentCategory === menuCategory && content.classList.contains("hidden")) {
+        content.classList.remove("hidden");
       }
+      if (contentCategory !== menuCategory && !content.classList.contains("hidden")){
+        content.classList.add("hidden");
+      }
+      console.log(content.classList)
     });
-
-    // munculkan lagi menu yg di-klik
-    menu.classList.remove("hiden");
   })
 })
 
+bottomMenuDiv.forEach(bottomMenu => {
+  bottomMenu.addEventListener("click", () => {
+    // reset scroll ke atas
+    document.documentElement.scrollTop = 0;
+
+    // get kategori bottomMenu: about / works / blog / contact
+    const bottomMenuCategory = bottomMenu.classList.item(1);
+
+    // hide semua content kecuali kategori yg di-klik
+    contentDiv.forEach(content => {
+      let contentCategory = content.classList.item(1);
+      if (contentCategory === bottomMenuCategory && content.classList.contains("hidden")) {
+        content.classList.remove("hidden");
+      } 
+      if(contentCategory !== bottomMenuCategory && !content.classList.contains("hidden")) {
+        content.classList.add("hidden");
+      }
+    });
+  })
+})
 
 
